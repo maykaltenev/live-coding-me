7f1a31507f004f0710166af3ef010744cc7cf74d
maykaltenev <michaeltenev@gmail.com>
2022-03-16 13:24:34 +0100

    JS

---
+++ mar/16-03/my-work/values.js
@@ -0,0 +1,71 @@
+// this is a comment 
+
+/* this is a 
+multi-line
+comment */
+
+// numbers
+2
+4
+9
+100
+    - 10
+
+// string
+"string"
+"this is a string"
+"this is another string"
+'this is a string'
+// '100'  this is a string
+
+// carlo said "hello"
+// "carlo sai "hello"" <-- this is wrong
+'carlo said "hello"'
+"this is a javascript string: 'string'"
+
+// booleans - logical operators
+true
+false
+
+// undefined 
+undefined
+
+// null
+null
+
+// a main difference between undefined and null:
+// - undefined is the unintentional lack of value
+// - null is intentional. We explicitly start with null,
+//  the same way we would start with a 0
+
+// Primitive values(primitive data types)
+
+// - number
+// - string
+// - boolean
+// - undefined
+// - null
+// - BigInt (we are not going to use this)
+// - Symbol (we are not going to use this)
+
+// Primitive values are immutable (they cannot be changed)
+
+
+
+// Objects (non-primitive values)
+//  - object:
+//      -- array
+//      -- date
+//      -- function
+
+// console.log(2);
+// console.log(10);
+// console.log(21);
+// console.log("hello");
+// console.log("100" + "5" - "5");
+// console.log(true);
+// console.log(undefined);
+// console.log(null);
+
+
+
