c10361edea2145f505bf67633dfe5c13ca341ef8
maykaltenev <michaeltenev@gmail.com>
2022-03-17 13:31:17 +0100

    17-03

---
+++ mar/17-03/my-work/00-recap.js
@@ -0,0 +1,9 @@
+// 3 ways of declaring variables;
+//  var ( we shouldn't use this)
+// let
+// const (this declares a constant variable)
+
+let number;
+console.log(number);
+number = 2;
+console.log(number);
---
+++ mar/17-03/my-work/01-naming-variables.js
@@ -0,0 +1,10 @@
+// naming convention in JavaScript is camelCase
+
+// JavaScript variable names are case sensitive it means that x is different than X
+
+const firstName = 'Carlo';
+const countryOfOrigin = 'Italy';
+const cityName = 'Berlin';
+const shoeSize = 45;
+const hairColor = 'black';
+const favoriteColor = 'green';
