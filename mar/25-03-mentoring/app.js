4de5d363d5ea88c53cb95b289d30e924873fdfe0
maykaltenev <michaeltenev@gmail.com>
2022-03-25 15:56:16 +0100

    sessions

---
+++ mar/25-03-mentoring/app.js
@@ -0,0 +1,18 @@
+// variables -> declaration, initialization
+
+// var type declaration | avoid them
+// 
+// let 
+// const 
+
+let x;
+console.log('x', x);
+console.log('hello world');
+
+// declaration
+const y = 5;
+console.log('y', y)
+
+// hoist ?
+console.log('z', z)
+var z = 0;
