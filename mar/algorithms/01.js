1c0822c0d1a3f69a49e62f4d8b6539e2b23e8be6
maykaltenev <michaeltenev@gmail.com>
2022-03-29 14:01:36 +0200

    algorithm

---
+++ mar/algorithms/01.js
@@ -0,0 +1,16 @@
+// Algorithm
+// * Clearly defined problem statement, input, and output;
+// * The steps in the algorithm need to be in a very specific order;
+// * The steps also need to be distinct;
+//  * The algorithm should produce a result;
+//  * The algorithm should complete in a finite amount of time;
+// Algorithmic Thinking:
+// clearly define what the problem set is and clarify what values count as inputs
+// the output should match the value we are looking for;
+// Type of algorithms
+// 1. linear search 
+//  - start at beginning
+//  - move sequently 
+//  - compare current value to target
+//  - reach end of list
+//NB! Algorithms should produce a result;
