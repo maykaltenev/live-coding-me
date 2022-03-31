1aa927662fb22f56967e4ab46fbe11b54a6ff8bc
maykaltenev <michaeltenev@gmail.com>
2022-03-31 12:01:43 +0200

    linked-list

---
+++ mar/data-structure/arrays.py
@@ -0,0 +1,21 @@
+new_list = [1,2,3];
+# result = new_list[0];
+# print(result);
+
+# if 1 in new_list: print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+    else:
+        print(False)
+
+
+
+numbers = [];
+len(numbers)
+
+numbers.append(2)
+numbers.append(200)
+numbers.extend([4, 5, 6]);
+print(numbers)
---
+++ mar/data-structure/data.js
@@ -0,0 +1,9 @@
+// Data Structure
+
+// a data storage format. It is the collection of values and the format they are store in the relationships between the values 
+// in the collection as well as the operations applied on the data stored in the structure
+
+let newList = [1, 2, 3];
+console.log(newList[1]);
+
+
