d41c1911048adcdfbf240cc5b9643e8b3c39c8b0
maykaltenev <michaeltenev@gmail.com>
2022-04-06 13:51:11 +0200

    05-04

--- apr/codewars/10_areaOfASquare.js
+++
@@ -1,6 +0,0 @@
-function squareArea(A) {
-    let radius = (A * 4) / (2 * Math.PI)
-    let areaSquare = Math.pow(radius, 2).toFixed(2);
-    return parseFloat(areaSquare)
-}
-console.log(squareArea(2))
--- apr/codewars/12_enumberableMagic.js
+++
@@ -1,7 +0,0 @@
-function eachCons(array, n) {
-    return array.map((x, y) => array.slice(y, y + n)).filter(x => x.length == n);
-}
-
-
-
-console.log(eachCons([1, 2, 3, 4], 4))
