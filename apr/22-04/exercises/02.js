7327c72600e5458c8ff4ff592a29222fda1df64b
maykaltenev <michaeltenev@gmail.com>
2022-04-22 17:48:34 +0200

    22-04

--- apr/22-04/exercises/02.js
+++ apr/22-04/exercises/02.js
@@ -51,23 +51,46 @@ const carts = [
     }
 ]
 // delete a product from the cart
-function deleteItem(object, user, product) {
-    let indexToDelete = -1;
-    for (let i = 0; i < carts.length; i++) {
-        if (object[i].user === user && object[i].product === product) {
-            indexToDelete = i;
-            console.log(carts[i])
+// function deleteItem(object, user, product) {
+//     let indexToDelete = -1;
+//     for (let i = 0; i < carts.length; i++) {
+//         if (object[i].user === user && object[i].product === product) {
+//             indexToDelete = i;
+//             console.log(carts[i])
+//         }
+//     }
+//     // with check on index
+//     // let result = [];
+//     // for (let i = 0; i < object.length; i++) {
+//     //     if (!(indexToDelete === i)) {
+//     //         result.push(object[i])
+//     //     }
+//     // }
+//     carts.splice(indexToDelete, 1);
+//     return carts;
+// }
+
+// console.log(deleteItem(carts, 'alkis', 'Bag'));
+
+// delete all the products of an object
+function deleteItem(object, product) {
+    let indexToDelete = [];
+    let result = [];
+    for (let i = 0; i < object.length; i++) {
+        if (object[i].product.toLowerCase().includes((product.toLowerCase()))) {
+            indexToDelete.push(i);
         }
     }
-    // with check on index
-    // let result = [];
-    // for (let i = 0; i < object.length; i++) {
-    //     if (!(indexToDelete === i)) {
-    //         result.push(object[i])
-    //     }
-    // }
-    carts.splice(indexToDelete, 1);
-    return carts;
+    console.log(indexToDelete)
+    for (let i = 0; i < object.length; i++) {
+        for (let j = 0; j < indexToDelete.length; j++) {
+            if (!indexToDelete[j] === i) {
+                result.push(object[j])
+            }
+        }
+    }
+    return result;
 }
 
-console.log(deleteItem(carts, 'alkis', 'Bag'));
+
+console.log(deleteItem(carts, 'Hat'));
