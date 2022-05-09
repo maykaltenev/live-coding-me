c6ad4d7c43b92d06dabbf192bd078e60acb35043
maykaltenev <michaeltenev@gmail.com>
2022-05-09 16:32:41 +0200

    array methods

--- may/09-05/array-methods-examples/start.js
+++ may/09-05/array-methods-examples/start.js
@@ -42,41 +42,63 @@ let products = [
 ];
 
 /* Map */
-
 // Capitalize the type of each product
-
 // Get an array containing only product types.
 // Example: ["T-shirt", "Socks", "Jeans", "T-shirt"]
-
+let capitalProduct = products.map(a => a.type.replace(a.type[0], a.type[0].toUpperCase()))
+console.log(capitalProduct)
 // Add a new property to the products describing the product
-
+products.map(a => a['description'] = `The ${a.color} ${a.type} costs $${a.price}!`);
+console.log(products)
 /* Filter and find */
 // Get an array with only yellow products
 
+let allYellow = products.filter(a => a.color === 'yellow');
+console.log(allYellow);
 // Get an array with products that cost more than 15
-
+let moreThan15 = products.filter(a => a.price > 15);
+console.log(moreThan15);
 // Get an array with listing only the types plus color of products that cost more than 15
+let productTypesMoreThan15 = products
+  .filter((product) => product.price > 15)
+  .map((product) => `${product.color} ${product.type}`);
+console.log(
+  `Products who cost more than 15`,
+  productTypesMoreThan15
+);
 
 
 // Get the first jeans in the products
-
-
+let firstJeans = products.find(a => a.type === 'jeans')
+console.log(firstJeans);
 // Check if there is at least a blue product
-
+let blueProduct = products.some(a => a.color === 'blue');
+console.log('Blue products', blueProduct)
 // Check if there is at least a pink product
-
+let pinkProduct = products.some(a => a.color === 'pink');
+console.log('The pink is', pinkProduct)
 
 // Check if there are at least 2 blue products
-
-
+let atLeast2 = products.filter(a => a.color === 'blue')
+  .some((a, index, array) => array.length >= 2)
+console.log('Check if there are at least 2 blue products', atLeast2)
 /* Reduce */
 
 // Calculate the total price of products
-
+let total = products.reduce((a, c) => a + c.price, 0);
+console.log(total)
 // Get an array with only product types, but without repetitions
+let types = [];
+products.map(a => {
+  if (!types.includes(a.type)) {
+    types.push(a.type);
+  }
+})
+console.log('types', types)
 
 // Get the most expensive product
-
+let [mostExpensiveProduct, ...rest] = products.sort((a, b) => b.price - a.price)
+console.log(mostExpensiveProduct)
 
 
 
