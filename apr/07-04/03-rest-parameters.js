722f6cc52f91aa688522663802c14e82353658ad
maykaltenev <michaeltenev@gmail.com>
2022-05-17 15:26:48 +0200

    17-05

--- apr/07-04/03-rest-parameters.js
+++ apr/07-04/03-rest-parameters.js
@@ -55,25 +55,23 @@ test(1, 2, 3, 4, 5, 6, 7, 8);
  * getMaxSmallerThanN(10, 100, 2000, 300, 40, 57) => null 
  */
 function getMaxSmallerThanN(n, ...numbers) {
-    let max = null;
-    for (let i = 0; i < numbers.length; i++) {
-        if ((max === null && numbers[i] < n) || (numbers[i] > max && numbers[i] < n)) {
-            max = numbers[i];
-        }
+  let max = null;
+  for (let i = 0; i < numbers.length; i++) {
+    if ((max === null && numbers[i] < n) || (numbers[i] > max && numbers[i] < n)) {
+      max = numbers[i];
     }
-    return max;
+  }
+  return max;
 }
-
 console.log(getMaxSmallerThanN(5, 1, 2, 3, 4, 5, 6));
 console.log(getMaxSmallerThanN(10, 100, 2000, 3, 40, 57));
 console.log(getMaxSmallerThanN(10, 100, 2000, 300, 40, 57))
 
 
 
-
 function testSpread(...rest) {
-    console.log(rest); // [1, 2, 3]
-    console.log(...rest); // after spreading: 1, 2, 3
+  console.log(rest); // [1, 2, 3]
+  console.log(...rest); // after spreading: 1, 2, 3
 }
 
 testSpread(1, 2, 3)
