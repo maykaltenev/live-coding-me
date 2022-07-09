6e52db9707664a5f6a500774d6dc5bf45d103715
maykaltenev <michaeltenev@gmail.com>
2022-07-09 15:15:54 +0200

    clean

--- jul/08-08/social-media-app/src/index.js
+++ jul/08-08/social-media-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-0000
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
