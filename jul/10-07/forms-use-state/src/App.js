fabc967e69d6dbcf68a7a0c5ca8403812de58423
maykaltenev <michaeltenev@gmail.com>
2022-07-10 10:39:08 +0200

    generic useState for forms

---
+++ jul/10-07/forms-use-state/src/App.js
@@ -0,0 +1,75 @@
+
+import React, { useState } from 'react';
+import './App.css';
+import map from "lodash/map"
+import startCase from "lodash/startCase"
+
+const Input = ({ label, id, handleChange, name, type, value }) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    <input type={type || "text"} id={id} name={name || id} onChange={handleChange} value={value} />
+    <br />
+  </>
+)
+
+const Radio = ({ label, id, handleChange, name, form }) => (
+  <>
+    <input
+      type="radio"
+      id={id}
+      name={name}
+      onChange={handleChange}
+      value={id}
+      checked={form[name] === id}
+    />
+    <label htmlFor={id}>{label}</label>
+    <br />
+  </>
+)
+
+const Checkbox = ({ label, id, handleChange, form }) => (
+  <>
+    <input
+      type="checkbox"
+      id={id}
+      name={id}
+      value={id}
+      onChange={handleChange}
+      checked={form[id]}
+    />
+    <label htmlFor={id}>{label}</label>
+    <br />
+  </>
+);
+function App() {
+  const [form, setFormValue] = useState({ name: "", weight: "", height: "", food: "", football: false, hockey: false })
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target;
+    setFormValue((prevFormValues) => ({
+      ...prevFormValues,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  }
+  return (
+    <>
+      {map(form, (val, key) => (
+        <p key={key}>
+          {`${startCase(key)}: `}
+          {`${val}`}
+        </p>
+      ))}
+      <Input label="Name: " id="name" handleChange={handleChange} value={form.name} />
+      <Input label="Weight: " id="weight" handleChange={handleChange} value={form.weight} />
+      <Input label="Height: " id="height" handleChange={handleChange} value={form.height} />
+      <p>I like to eat:</p>
+      <Radio form={form} name="food" label="Salad" id="salad" handleChange={handleChange} />
+      <Radio form={form} name="food" label="Steak" id="steak" handleChange={handleChange} />
+      <Radio form={form} name="food" label="Salad with steak" id="Salad with Steak" handleChange={handleChange} />
+      <p>I like the following</p>
+      <Checkbox form={form} label="Football" id="football" handleChange={handleChange} />
+      <Checkbox form={form} label="Hockey" id="hockey" handleChange={handleChange} />
+    </>
+  );
+}
+
+export default App;
