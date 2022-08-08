2db55fd5d2ff9558ec1157038a84b86d1db5a38d
maykaltenev <michaeltenev@gmail.com>
2022-08-09 00:18:50 +0200

    form

--- aug/08-08/hello-world-react/src/App.js
+++ aug/08-08/hello-world-react/src/App.js
@@ -8,10 +8,15 @@ import List from "./components/List";
 const list = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState({ firstName: '', lastName: '' })
   const handleClick = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
+    alert(`My name is ${name.firstName}  ${name.lastName} `)
   };
-
+  const handleChange = (e) => {
+    setName({ ...name, [e.target.name]: [e.target.value] })
+    console.log(name)
+  }
   return (
     <div className="App">
       <div className='center'>
@@ -20,6 +25,11 @@ function App() {
       <button onClick={handleClick}>Click</button>
       <div>{count}</div>
       <List list={list} />
+      <form onChange={handleChange}>
+        <input type='text' name="firstName" placeholder="First name"></input>
+        <input type='text' name="lastName" placeholder="Last name" ></input>
+        <button onClick={handleClick}>Greeting</button>
+      </form>
     </div >
   );
 }
