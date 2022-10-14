80221ca63180e116ac8fc1a538e7364221adf35f
Maykal Tenev <michaeltenev@gmail.com>
2022-10-14 13:42:37 +0200

    update

---
+++ oct/14-10-mentoring/client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Route } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <Route path='/' element={<App />} />
+    </BrowserRouter>
+);
